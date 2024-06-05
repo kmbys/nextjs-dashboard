3ec9f82afa4445f15ec394fa3a7bc89a9824de39
kmbys <55523468+kmbys@users.noreply.github.com>
2024-06-05 16:58:49 +0900

    add authentication

---
+++ auth.config.ts
@@ -0,0 +1,19 @@
+import { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      if (nextUrl.pathname.startsWith('/dashboard')) {
+        return isLoggedIn;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
